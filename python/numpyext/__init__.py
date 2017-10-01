3946b6fafb27bc9bfdedf39da91e395fcfb27bb0
Ian Lumsden <lumsden.ian@gmail.com>
2017-09-30 22:04:55 -0400

    Adds Python 3 compatibility to this file.

--- python/numpyext/__init__.py
+++ python/numpyext/__init__.py
@@ -17,20 +17,20 @@ def copyright():
 
 import numpyext as binding
 
-def getdataptr( npyarr ):
+def getdataptr(npyarr):
     '''extract data pointer from a numpy array and return a PyCObject'''
-    return binding.getdataptr( npyarr )
+    return binding.getdataptr(npyarr)
 
 
-def wrapdataptr( ptr, dtype, size ):
+def wrapdataptr(ptr, dtype, size):
     '''wrap a void pointer in the given PyCObject into  a numpy array
     dtype: type code or numpy dtype instance
     size: size of array
     '''
-    if not isinstance( dtype, int ):
+    if not isinstance(dtype, int):
         dtype = dtype.num
         pass
-    return binding.wrapdataptr( ptr, dtype, size )
+    return binding.wrapdataptr(ptr, dtype, size)
         
 
 # version
